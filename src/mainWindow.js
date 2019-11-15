c8e153d6b8a9a340131b040347d3d9f13ce11c5a
Jack Evans <jack.evans@msservicefortressgb.onmicrosoft.com>
2019-11-15 16:24:07 +0000

    Toggle border Background starts as #fff

--- src/mainWindow.js
+++ src/mainWindow.js
@@ -1,36 +1,36 @@
-const remote = require('electron').remote
+const remote = require("electron").remote;
 
 $(document)
-    .ready(function () {
-        $banner = $('#banner-message')
-        changeColor($banner, randomColor())
-
-    })
-    .on('click', '#close', function () {
-        remote.BrowserWindow.getFocusedWindow().close()
-    })
-    .on('click', '#maximise', function () {
-        var window = remote.BrowserWindow.getFocusedWindow()
-        if (window.isMaximized())
-            window.unmaximize()
-        else
-            window.maximize()
-    })
-    .on('click', '#minimise', function () {
-        remote.BrowserWindow.getFocusedWindow().minimize()
-    })
-    .on('click', '#toggle-text', function () {
-        $('#title-text').toggle()
-        $('.menu').toggleClass('invisible')
-    })
+  .ready(function() {
+    $banner = $("#banner-message");
+    changeColor($banner, randomColor());
+  })
+  .on("click", "#close", function() {
+    remote.BrowserWindow.getFocusedWindow().close();
+  })
+  .on("click", "#maximise", function() {
+    var window = remote.BrowserWindow.getFocusedWindow();
+    if (window.isMaximized()) window.unmaximize();
+    else window.maximize();
+  })
+  .on("click", "#minimise", function() {
+    remote.BrowserWindow.getFocusedWindow().minimize();
+  })
+  .on("click", "#toggle-text", function() {
+    $("#title-text").toggle();
+    $(".menu").toggleClass("invisible");
+  })
+  .on("click", "#toggle-border", function() {
+    $("#banner-message").toggleClass("border");
+  });
 
 function changeColor($this, color) {
-    $this.css('background', color);
-    setTimeout(function () {
-        changeColor($this, randomColor())
-    }, 5000)
+  $this.css("background", color);
+  setTimeout(function() {
+    changeColor($this, randomColor());
+  }, 5000);
 }
 
 function randomColor() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
